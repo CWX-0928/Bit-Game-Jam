80a89263fb68f23da564ac96944b26c122ef227d
lcychengyang-a11y <lcychengyang@gmail.com>
2026-05-27 20:07:11 +0800

    Add files via upload

---
+++ Bit-Game-Jam/EnvironmentAsset/character_body_2d.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed := 200.0
+
+func _physics_process(delta):
+	var input_direction = Vector2.ZERO
+	input_direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_direction = input_direction.normalized()
+	velocity = input_direction * speed
+	move_and_slide()
+	
