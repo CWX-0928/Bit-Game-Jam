39635c7aa50a314edf5ab4c113c9e810b2cc2e6e
CWX-0928 <chinwxuan0928@gmail.com>
2026-05-28 23:35:33 +0800

    added fireball cooldown and credit scene

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var sprite_animation: AnimationPlayer = $"Animated Sprite/Sprite_animation"
+@onready var sprite_animation: AnimationPlayer = $"Background/Animated Sprite/Sprite_animation"
 @onready var theme_animation: AnimationPlayer = $Theme_animation
 
 func _ready() -> void:
@@ -11,8 +11,9 @@ func _ready() -> void:
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://node_2d.tscn")
 	
-func _on_setting_pressed() -> void:
-	get_tree().change_scene_to_file("res://setting.tscn")
-
+func _on_credit_pressed() -> void:
+	get_tree().change_scene_to_file("res://credit.tscn")
+	
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+	
