668576df285c8c1785a0d075b777506c7cfcf729
CWX-0928 <chinwxuan0928@gmail.com>
2026-05-29 16:19:34 +0800

    cd attach to main scene & main menu fixed?

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animation_player: AnimationPlayer = $cd/AnimationPlayer
+
 @export var speed  = 200
 
 var max_LIFE = 100
@@ -21,13 +23,12 @@ func handleInput():
 func _input(_event):
 	if Input.is_action_just_pressed("click") and can_attack:
 		get_node("Magicspawn").shoot()
-		can_attack = false
+		animation_player.play("cd_animation")
 		await get_tree().create_timer(1).timeout
 		can_attack = true
 	else:
+		can_attack = false
 		return
-
-
 
 func _process(_delta: float) -> void:
 	handleInput()
